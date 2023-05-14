68e2c5ba3c28d01f5fccec065fbeafc1306cff41
blackair23 <deivid96iliev@gmail.com>
2023-05-14 11:29:38 +0300

    Order Print with React-to-print

--- client/src/Components/Print/Print.js
+++ client/src/Components/Print/Print.js
@@ -1,8 +1,9 @@
 import { doc, getDoc } from 'firebase/firestore';
 import styles from './Print.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { db } from '../../config/firebase';
+import { useReactToPrint } from 'react-to-print';
 import swal from 'sweetalert';
 
 export const Print = () => {
@@ -49,8 +50,16 @@ export const Print = () => {
         } catch (error) {
           console.error(error);
         }
-      };
-
+    };
+    
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+        documentTitle: `${order && order.city}_${order && order.timestamp.toDate().getDate()}.${ order? order.timestamp.toDate().getMonth() + 1 : ""}.${order? order.timestamp.toDate().getFullYear() : ""}`,
+        onAfterPrint: () => swal('Print succesful', {
+            icon: "success",
+        })
+    })
 
     useEffect(() => {
         if(order){
@@ -61,81 +70,84 @@ export const Print = () => {
         
     
     return (
-        <section id={styles.print}>
-            <div className={styles.page} size="A4">
-        <h2>ПРИЕМО-ПРЕДАВАТЕЛЕН ПРОТОКОЛ</h2>
-        <p className={styles.paragraph}>Днес  {order && order.timestamp.toDate().getDate()}/{ order? order.timestamp.toDate().getMonth() + 1 : ""}/{order? order.timestamp.toDate().getFullYear() : ""}г.
-         в гр. София, долуподписаният представител на „...” – 
-            офис {order && order.city} получи от представител на „...” 
-            – Генерална Дирекция - следните заявления за застраховане и документи към тях:</p>
-        
-        <table>
-            <tbody>
-            <tr>
-                <th scope="col">N:</th>
-                <th scope="col">Вид:</th>
-                <th scope="col">Пореден номер от:</th>
-                <th scope="col">До:</th>
-                <th scope="col">брой:</th>
-            </tr>
-            {/* eslint-disable  */}
-                {products.length > 0 ? 
-                products.map((p, i) => {
-                    console.log('---------map---------',p )
-                    let index = '';
-                    let startNumber = Number(p.startNumber);
-                    let usedToNow = 0;
-                    let from = 0;
-                    // if(p.document){
-                        index = p.array.findIndex(object => {
-                            return object.id === id;
-                        });
-                        console.log('---------documents---------',index )
-                        for (let i = 0; i <= index; i++) {
-                            usedToNow += Number(p.array[i].quantity);
-                            if(i === index - 1){
-                                console.log(i , '<==>', index-1)
-                                from = Number(usedToNow)
+        <div className={styles.bg}>
+            <section ref={componentRef} id={styles.print}>
+                <div className={styles.page} size="A4">
+            <h2>ПРИЕМО-ПРЕДАВАТЕЛЕН ПРОТОКОЛ</h2>
+            <p className={styles.paragraph}>Днес  {order && order.timestamp.toDate().getDate()}/{ order? order.timestamp.toDate().getMonth() + 1 : ""}/{order? order.timestamp.toDate().getFullYear() : ""}г.
+            в гр. София, долуподписаният представител на „...” – 
+                офис {order && order.city} получи от представител на „...” 
+                – Генерална Дирекция - следните заявления за застраховане и документи към тях:</p>
+            
+            <table>
+                <tbody>
+                <tr>
+                    <th scope="col">N:</th>
+                    <th scope="col">Вид:</th>
+                    <th scope="col">Пореден номер от:</th>
+                    <th scope="col">До:</th>
+                    <th scope="col">брой:</th>
+                </tr>
+                {/* eslint-disable  */}
+                    {products.length > 0 ? 
+                    products.map((p, i) => {
+                        console.log('---------map---------',p )
+                        let index = '';
+                        let startNumber = Number(p.startNumber);
+                        let usedToNow = 0;
+                        let from = 0;
+                        // if(p.document){
+                            index = p.array.findIndex(object => {
+                                return object.id === id;
+                            });
+                            console.log('---------documents---------',index )
+                            for (let i = 0; i <= index; i++) {
+                                usedToNow += Number(p.array[i].quantity);
+                                if(i === index - 1){
+                                    console.log(i , '<==>', index-1)
+                                    from = Number(usedToNow)
+                                }
+                                console.log(usedToNow);
                             }
-                            console.log(usedToNow);
-                        }
-                    // }
-                    let currentQuantity = p.array[index].quantity;
+                        // }
+                        let currentQuantity = p.array[index].quantity;
 
-                    return (
-                        <tr key={p._id} scope="row">
-                            <td data-label="N">{ i + 1 }</td>
-                            <td data-label="Вид">{ p.name }</td>
-                            <td data-label="Пореден номер от">{ p.document ? startNumber + from : "-" }</td>
-                            <td data-label="До">{ p.document ? startNumber + usedToNow : "-" }</td>
-                            <td data-label="брой">{ currentQuantity }</td>
-                        </tr>
-                    )})
-                :
-                ''
-                }
-            </tbody>
-        </table>
+                        return (
+                            <tr key={p._id} scope="row">
+                                <td data-label="N">{ i + 1 }</td>
+                                <td data-label="Вид">{ p.name }</td>
+                                <td data-label="Пореден номер от">{ p.document ? startNumber + from : "-" }</td>
+                                <td data-label="До">{ p.document ? startNumber + usedToNow : "-" }</td>
+                                <td data-label="брой">{ currentQuantity }</td>
+                            </tr>
+                        )})
+                    :
+                    ''
+                    }
+                </tbody>
+            </table>
 
-            <div className={styles.sign}>
-                <div className={styles.left}>
-                    <p>
-                        ПРЕДАЛ за ГРАВЕ - Централа:	
-                    </p>
-                    <p>
-                        /.............................................../
-                    </p>
+                <div className={styles.sign}>
+                    <div className={styles.left}>
+                        <p>
+                            ПРЕДАЛ за ГРАВЕ - Централа:	
+                        </p>
+                        <p>
+                            /.............................................../
+                        </p>
+                    </div>
+                    <div className={styles.right}>
+                        <p>
+                            ПРИЕЛ за офис {order && order.city}:	
+                        </p>
+                        <p>
+                            /............................................./
+                        </p>
+                    </div>
                 </div>
-                <div className={styles.right}>
-                    <p>
-                        ПРИЕЛ за офис {order && order.city}:	
-                    </p>
-                    <p>
-                        /............................................./
-                    </p>
                 </div>
-            </div>
-            </div>
-        </section>
+            </section>
+            <button className={styles.printBtn} onClick={handlePrint} >Print</button>
+        </div>
     )
 }
